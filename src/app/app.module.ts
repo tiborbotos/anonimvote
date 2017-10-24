2fb3c4d101def561e45533f0de250297c8096fd5
Tibor Botos <tiborbotos@Tibor-MacBook-Pro.local>
2017-10-24 20:58:10 +0200

    Proper readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { EditorItemComponent } from './editor-item/editor-item.component';
+import { AdminComponent } from './admin/admin.component';
+import { VotingComponent } from './voting/voting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    EditorItemComponent,
+    AdminComponent,
+    VotingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/editor-item/editor-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vt-editor-item',
+  templateUrl: './editor-item.component.html',
+  styleUrls: ['./editor-item.component.css']
+})
+export class EditorItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
